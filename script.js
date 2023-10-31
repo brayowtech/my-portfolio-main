7147188633acebf7cc5e3c18898adb8b682a6df3
BrayowTech Developers <nachokuya@gmail.com>
2023-10-31 21:42:31 +0300

    script.js working tree

--- script.js
+++ script.js
@@ -12,29 +12,19 @@ faXmark.onclick = function(){
 }
 
 // Google sheets form submission
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwmoxHv2iRAtZk51Qhl8oD6DmJivkeesW7qqsLS1gygI8W5Wpf-G3mAfN32YEXMkzOqTQ/exec'
-  const form = document.forms['submit-to-google-sheet'];
-  const nameInput = document.getElementById("nameInput");
-  const msg = document.getElementById("msg");
-  
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwDYDg1eH2Pi_iH05E1gILo-QqHXNaoVElcOLoWec3yDm3TsME7sIfqLP2FEqcfoDxz_Q/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+
   form.addEventListener('submit', e => {
-      e.preventDefault();
-  
-      // Create a FormData object and include the name input value
-      const formData = new FormData(form);
-      const name = nameInput.value; // Get the name input value
-  
-      formData.append('name', name); // Add the name to the FormData
-  
-      // Send the data to Google Sheets
-      fetch(scriptURL, { method: 'POST', body: formData })
-          .then(response => {
-              msg.innerHTML = "Thank you, your response has been submitted";
-              setTimeout(() => {
-                  msg.innerHTML = "";
-              }, 5000);
-              form.reset();
-          })
-          .catch(error => console.error('Error!', error.message));
-  });
-  
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response =>{
+        msg.innerHTML = "Thank you, your response has been submitted";
+        setTimeout(() => {
+          msg.innerHTML = "";
+        }, 5000);
+        form.reset();
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
