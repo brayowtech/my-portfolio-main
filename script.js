cb48b0e8ce7631c2e3d92bffcd880e27695f0f9b
BrayowTech Developers <nachokuya@gmail.com>
2023-11-08 15:58:15 +0300

    Script

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ faXmark.onclick = function(){
 }
 
 // Google sheets form submission
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbyQhDKIzMA8Y7tch5ie-0NlJGtUK9ECpaqFYI1ohTNOa0SEvGAo4ayd3-6ZDnles8om/exec'
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwlLB3v-TKHROjGkJRlEKUQPG6ugDVcu4d_GB2BZZo6iYa_J34MZ6MsZ5z2MioQwS6L/exec'
   const form = document.forms['submit-to-google-sheet']
   const msg = document.getElementById("msg")
 
